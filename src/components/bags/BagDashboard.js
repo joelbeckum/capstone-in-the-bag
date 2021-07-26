2e2d1ff5e620b7bfd2f8949e7302a7719240e584
Joel Beckum <joelbeckum@gmail.com>
2021-07-26 11:17:49 -0400

    Layed out basic dashboard and implemented navbar; logo onClick will return user to dashboard

--- src/components/bags/BagDashboard.js
+++ src/components/bags/BagDashboard.js
@@ -1,10 +1,12 @@
 import React, { useContext, useEffect } from "react"
+import { useHistory } from "react-router-dom"
 import { BagContext } from "./BagProvider"
 import { Bag } from "./Bag"
 import "./Bags.css"
 
 export const BagDashboard = () => {
     const { bags, getBags } = useContext(BagContext)
+    const history = useHistory()
 
     useEffect(() => {
         getBags()
@@ -16,7 +18,7 @@ export const BagDashboard = () => {
 
     return (
         <>
-            <h1 className="title">Your Bags</h1>
+            <h1 className="bags__title">Your Bags</h1>
             <section className="dashboard">
                 <div className="bags__wrapper">
                     {
@@ -26,7 +28,14 @@ export const BagDashboard = () => {
                     }
                 </div>
                 <div className="buttons__wrapper">
-
+                    <div className="button bags__new" onClick={() => history.push("/bags/create")}>
+                        <img src="https://via.placeholder.com/115x130" alt="placeholder image"/>
+                        Create a Bag
+                    </div>
+                    <div className="button bags__friends">
+                        <img src="https://via.placeholder.com/115x130" alt="placeholder image"/>
+                        See Others' Bags
+                    </div>
                 </div>
             </section>
         </>
