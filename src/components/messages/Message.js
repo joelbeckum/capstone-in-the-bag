e534ef85ef52e165c3315814ae451bda4100a0d2
Joel Beckum <joelbeckum@gmail.com>
2021-07-28 15:32:28 -0400

    added delete affordances to messages

--- src/components/messages/Message.js
+++ src/components/messages/Message.js
@@ -1,8 +1,9 @@
-import React from "react"
+import React, { useContext } from "react"
+import { MessageContext } from "./MessageProvider"
 import "../bags/Bags.css"
 
 export const Message = ({ message }) => {
-    
+    const { removeMessage } = useContext(MessageContext)
     
     return (
         <div className="message">
@@ -11,6 +12,9 @@ export const Message = ({ message }) => {
             </div>
             <div className="message__body">
                 {message.body}
+            </div>
+            <div className="message__delete" onClick={() => removeMessage(message.id)}>
+                <img src="https://via.placeholder.com/25x25" alt="placeholder"/>
             </div>
         </div>
     )
