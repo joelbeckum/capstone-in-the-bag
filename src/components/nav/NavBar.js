5338a3dd89d2de7f911bd2d401884211b251ae50
Joel Beckum <joelbeckum@gmail.com>
2021-07-30 11:17:03 -0400

    Refactored logout button in nav bar

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,26 +1,40 @@
 import React from "react"
 import { Link, useHistory } from "react-router-dom"
+import logo from "../../icons/logo.png"
 import "./NavBar.css"
 
 export const NavBar = (props) => {
     const history = useHistory()
 
+    const handleClickLogout = e => {
+        e.preventDefault()
+
+        sessionStorage.removeItem("itb_user")
+        history.push("/login")
+    }
+
     return (
         <nav className="navbar">
-            <div className="logo" onClick={() => history.push("/")}>
-                <img src="https://via.placeholder.com/75x75" alt="placeholder"/>
-            </div>
-            <div className="logout">
-                <Link
-                    className="nav-link"
-                    to="/login"
-                    onClick={() =>
-                        sessionStorage.removeItem("itb_user")
-                    }
-                >
-                    Logout
-                </Link>
+            <div className="navbar__wrapper">
+                <div className="logo" onClick={() => history.push("/")}>
+                    <img src={logo} alt="placeholder"/>
+                </div>
+                <div className="logout">
+                    <button className="logout__button" onClick={handleClickLogout}>
+                        LOG OUT
+                    </button>
+                </div>
             </div>
         </nav>
     )
 }
+
+{/* <Link
+                        className="nav-link"
+                        to="/login"
+                        onClick={() =>
+                            sessionStorage.removeItem("itb_user")
+                        }
+                    >
+                        Logout
+                    </Link> */}
